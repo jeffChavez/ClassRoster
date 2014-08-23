a523992c9cc6e7eafe4a04e07f0458841cbdd6e9
Jeff Chavez <Maluu@Jeffs-MacBook-Pro.local>
2014-08-23 16:08:22 -0700

    Initial Commit

---
+++ Class Roster Part 5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Class Roster Part 5
+//
+//  Created by Jeff Chavez on 8/23/14.
+//  Copyright (c) 2014 Jeff Chavez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
